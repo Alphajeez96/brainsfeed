fc392caf7e0227a02c3efaaac4e4785fa70b3938
Prince <chimeremezechukwudire@gmail.com>
2020-07-28 21:47:37 +0100

    project structure added

---
+++ src/components/Home/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello There</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {},
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+const base = axios.create({
+    baseURL: "https://api.jsonbin.io/b/5f20829bc58dc34bf5dca275",
+});
 
 Vue.config.productionTip = false
+Vue.prototype.$http = base;
+Vue.use(VueAxios, axios);
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
