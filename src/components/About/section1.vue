0a2f804ce6361bb58adaecfe962e3612d03df03f
Prince <chimeremezechukwudire@gmail.com>
2020-08-01 02:58:33 +0100

    card optimized

--- src/components/About/section1.vue
+++ src/components/About/section1.vue
@@ -1,129 +1,128 @@
 <template>
+  <div>
     <div>
-    <div>
-    <!-- <h1>{{ companies ? companies.specialties : 'Not available' }}</h1> -->
-    <div class="body">
-      <div class="container px-0">
-        <div class="row">
-          <!-- Details holder here -->
-          <div class="col-md-8">
-            <div class="row white__bg">
-              <!-- Image/screenshot here -->
-              <div class="col-md-6 img__holder">
-                <div>
-                  <img
-                    :src="`${companies.screenshot}`"
-                    class="img-fluid"
-                    alt=""
-                  />
+      <!-- <h1>{{ companies ? companies.specialties : 'Not available' }}</h1> -->
+      <div class="body">
+        <div class="container px-0">
+          <div class="row">
+            <!-- Details holder here -->
+            <div class="col-md-8">
+              <div class="row white__bg">
+                <!-- Image/screenshot here -->
+                <div class="col-md-6 img__holder">
+                  <div>
+                    <img :src="`${companies.screenshot}`" class="img-fluid" alt />
+                  </div>
                 </div>
-              </div>
 
-              <div class="col-md-6">
-                <div class="details">
-                  <h4>{{ companies ? companies.title_ : 'Not available' }}</h4>
-                
-                  <h5 class="pt-3">
-                    headquaters:
-                    <span>
-                      {{ companies ? companies.headquarters : 'Not available' }}
-                    </span>
-                  </h5>
-                  <h5 class="pt-3">
-                    Industry:
-                    <span>
-                      {{ companies ? companies.industry : 'Not available' }}
-                    </span>
-                  </h5>
-                  <h5 class="pt-3">
-                    Address :
-                    <span>
-                      {{
+                <div class="col-md-6">
+                  <div class="details">
+                    <h4>{{ companies ? companies.title_ : 'Not available' }}</h4>
+
+                    <h5 class="pt-3">
+                      headquaters:
+                      <span>{{ companies ? companies.headquarters : 'Not available' }}</span>
+                    </h5>
+                    <h5 class="pt-3">
+                      Industry:
+                      <span>{{ companies ? companies.industry : 'Not available' }}</span>
+                    </h5>
+                    <h5 class="pt-3">
+                      Address :
+                      <span>
+                        {{
                         companies ? companies.companyAddress : 'Not available'
-                      }}
-                    </span>
-                  </h5>
-  <h5 class="pt-3">
-                    Founded:
-                    <span>
-                      {{ companies ? companies.founded : 'Not available' }}
-                    </span>
-                  </h5>
-                  <div class="inline pt-3">
-                    <p>
-                      Rating:
-                      {{ companies ? companies.rating : 'Not available' }}
-                    </p>
-                    <p class="mx-4">
-                      Price: {{ companies ? companies.price : 'Not available' }}
-                    </p>
+                        }}
+                      </span>
+                    </h5>
+                    <h5 class="pt-3">
+                      Founded:
+                      <span>{{ companies ? companies.founded : 'Not available' }}</span>
+                    </h5>
+                    <div class="inline pt-3">
+                      <p>
+                        Rating:
+                        {{ companies ? companies.rating : 'Not available' }}
+                      </p>
+                      <p class="mx-4">Price: {{ companies ? companies.price : 'Not available' }}</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <!-- Mini side bar here -->
-          <div class="col-md-4 white__bg">
-            <h4>Company Info</h4>
-            <div>
-              <img :src="`${companies.logo}`" alt="" />
-            </div>
-            <div>
-              <span class="badge badge-pill mt-3 badge-success">
-                {{ companies ? companies.type : 'Not available' }}
-              </span>
-            </div>
-            <div>
-              <h6 class='mt-3 '>
-                {{ companies ? companies.company_name : 'Not available' }}
-              </h6>
-            </div>
-            <div>
-              <h6 class='mt-3 '>
-                {{ companies ? companies.company_size : 'Not available' }}
-              </h6>
-            </div>
+            <!-- Mini side bar here -->
+            <div class="col-md-4 white__bg">
+              <h4>Company Info</h4>
+              <div>
+                <img :src="`${companies.logo}`" alt />
+              </div>
+              <div>
+                <span
+                  class="badge badge-pill mt-3 badge-success"
+                >{{ companies ? companies.type : 'Not available' }}</span>
+              </div>
+              <div>
+                <h6 class="mt-3">{{ companies ? companies.company_name : 'Not available' }}</h6>
+              </div>
+              <div>
+                <h6 class="mt-3">{{ companies ? companies.company_size : 'Not available' }}</h6>
+              </div>
 
-            <div>
-            
-               <h6>{{ companies ? companies.website_ : 'Not available' }}</h6>
+              <div>
+                <a
+                  :href="'' + companies.website_ "
+                  target="_blank"
+                >{{ companies ? companies.website_ : 'Not available' }}</a>
+              </div>
+              <div class="social__holder mt-3">
+                <a
+                  :href="'' + companies.facebookUrl "
+                  target="_blank"
+                >  <font-awesome-icon class='mx-2' :icon="{ prefix: 'fab', iconName: 'facebook' }"/></a>
+
+                    <a
+                  :href="'' + companies.twitterUrl "
+                  target="_blank"
+                >  <font-awesome-icon class='mx-2' :icon="{ prefix: 'fab', iconName: 'twitter' }"/></a>
+
+                       <a
+                  :href="'' + companies.linkedinUrl "
+                  target="_blank"
+                >  <font-awesome-icon class='mx-2' :icon="{ prefix: 'fab', iconName: 'linkedin' }"/></a>
+                 
+                   <a
+                  :href="'mailto:' + companies.email "
+                  target="_blank"
+                >  <font-awesome-icon class='mx-2' :icon="{ prefix: 'fa', iconName: 'envelope' }"/></a>
+
+                   
+                             
+              </div>
             </div>
-            <div class="social__holder">
-                <font-awesome-icon icon="user-secret" />
-                  
-                    
- 
-                </div>
-       
           </div>
         </div>
       </div>
     </div>
-    </div>
-    </div>
-   
+  </div>
 </template>
 <script>
-
-
-
-const axios = require('axios')
+const axios = require("axios");
 const About = {
-  props: ['companyid'],
-}
+  props: ["companyid"],
+};
 
 export default {
-  name: '',
+  name: "",
   data() {
     return {
-      secretKey: '$2b$10$Wb8VppNIDoAhIAB8n8oFnuCuOnANGbL7pxknMH8lhYplG40/hi5LC',
+      secretKey: "$2b$10$Wb8VppNIDoAhIAB8n8oFnuCuOnANGbL7pxknMH8lhYplG40/hi5LC",
       companies: null,
       router: this.$route.params.companyid,
-    }
+    };
   },
   mounted() {
-    this.getCompany()
+    this.getCompany();
   },
   methods: {
     async getCompany() {
@@ -132,24 +131,24 @@ export default {
           `https://api.jsonbin.io/b/5f20829bc58dc34bf5dca275`,
           {
             headers: {
-              'secret-key': this.secretKey,
+              "secret-key": this.secretKey,
             },
-          },
-        )
+          }
+        );
 
-        console.log(this.router)
+        console.log(this.router);
 
-        console.log(response.data)
+        console.log(response.data);
         this.companies = response.data.find(
-          (company) => company.objectID === this.router,
-        )
-        console.log(this.companies)
+          (company) => company.objectID === this.router
+        );
+        console.log(this.companies);
       } catch (error) {
-        console.log(error.response)
+        console.log(error.response);
       }
     },
   },
-}
+};
 </script>
 <style scoped>
 .body {
@@ -173,25 +172,31 @@ export default {
 
 .inline p {
   display: inline;
-
 }
 
-h4{
+h4 {
   font-size: 20px;
-
 }
-h5{
+h5 {
   font-size: 16px;
 }
- .pt-3 span{
+.pt-3 span {
   font-size: 14px;
   line-height: 22px;
 }
 a {
   text-decoration: none !important;
 }
-h3{
-font-size: 22px;
-margin-top: 20px;}
+h3 {
+  font-size: 22px;
+  margin-top: 20px;
+}
+path{
+  color: black;
+  height: 4vh
 
+}
+path :hover{
+  color: blue
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -7,16 +7,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import VueAxios from "vue-axios";
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret, } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faTwitter, faLinkedin, } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import Paginate from "vuejs-paginate";
 
 
 
-library.add(faUserSecret, )
+
+library.add(faFacebook, faLinkedin, faTwitter, faEnvelope)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-Vue.component("paginate", Paginate);
+
 
 const base = axios.create({
     baseURL: "https://api.jsonbin.io/b/5f20829bc58dc34bf5dca275",
